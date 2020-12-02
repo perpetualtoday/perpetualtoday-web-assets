9e80498d2467b428a5039bf25dbe907c57a65570
Your Name <you@example.com>
2020-12-02 19:12:42 +0800

    Add more no-reload functions

--- js/script-noreload.js
+++ js/script-noreload.js
@@ -3,3 +3,23 @@ $('#onepress-free-main-menu .widget').addClass('show-menu');
 $('.search-toggle').on('click', function () {
     $('body').toggleClass('search-active')
 });
+    $('#onepress-free-mobile-menu').each(function () {
+        var onepressMoreLink = $(this),
+            onepressMoreLinkContent4 = $('#onepress-free-main-menu-nav').clone();
+        onepressMoreLinkContent4.attr('id', 'main-mobile-nav');
+        onepressMoreLinkContent4.appendTo(onepressMoreLink);
+        $('.show-onepress-free-mobile-menu, .hide-onepress-free-mobile-menu, .overlay').on('click', function () {
+            $('body').toggleClass('nav-active')
+        });
+        $('.onepress-free-mobile-menu .has-sub').append('<div class="submenu-toggle"/>');
+        $('.onepress-free-mobile-menu ul li .submenu-toggle').on('click', function (onepressAuthorNames) {
+            if ($(this).parent().hasClass('has-sub')) {
+                onepressAuthorNames.preventDefault();
+                if (!$(this).parent().hasClass('show')) {
+                    $(this).parent().addClass('show').children('.m-sub').slideToggle(170)
+                } else {
+                    $(this).parent().removeClass('show').find('> .m-sub').slideToggle(170)
+                }
+            }
+        })
+    });
--- js/script.js
+++ js/script.js
@@ -427,26 +427,6 @@ $(function () {
         onepressMoreLinkContent3.find('#h1-tag').remove();
         onepressMoreLinkContent3.appendTo(onepressMoreLink)
     });
-    $('#onepress-free-mobile-menu').each(function () {
-        var onepressMoreLink = $(this),
-            onepressMoreLinkContent4 = $('#onepress-free-main-menu-nav').clone();
-        onepressMoreLinkContent4.attr('id', 'main-mobile-nav');
-        onepressMoreLinkContent4.appendTo(onepressMoreLink);
-        $('.show-onepress-free-mobile-menu, .hide-onepress-free-mobile-menu, .overlay').on('click', function () {
-            $('body').toggleClass('nav-active')
-        });
-        $('.onepress-free-mobile-menu .has-sub').append('<div class="submenu-toggle"/>');
-        $('.onepress-free-mobile-menu ul li .submenu-toggle').on('click', function (onepressAuthorNames) {
-            if ($(this).parent().hasClass('has-sub')) {
-                onepressAuthorNames.preventDefault();
-                if (!$(this).parent().hasClass('show')) {
-                    $(this).parent().addClass('show').children('.m-sub').slideToggle(170)
-                } else {
-                    $(this).parent().removeClass('show').find('> .m-sub').slideToggle(170)
-                }
-            }
-        })
-    });
     $('.social-mobile').each(function () {
         var onepressMoreLink = $(this),
             onepressMoreLinkContent3 = $('#navbar-social ul.social').clone();
