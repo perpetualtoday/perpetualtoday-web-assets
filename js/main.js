ccfec8d10956b90ec79ae4e519c060af147ca6b0
John Vincent Corcega <corcegajovic@gmail.com>
2020-12-03 17:41:30 +0800

    Put disqus load scripts to ajax loading

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     'use strict';
 
     function loadCommentsScript() {
-        var len = $('script').filter(function () {
+        var len = $('script').filter(function() {
             return ($(this).attr('src') == 'https://www.blogger.com/static/v1/jsbin/1875144490-comment_from_post_iframe.js');
         }).length;
 
@@ -13,7 +13,34 @@
     }
 
     function loadComments() {
-        (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(bloggerjs);
+
+        var disqus_shortname = "perpetualtoday";
+        var disqus_blogger_current_url = "<data:blog.canonicalUrl/>";
+
+        if (!disqus_blogger_current_url.length) {
+            disqus_blogger_current_url = "<data:blog.url/>";
+        }
+
+        var disqus_blogger_homepage_url = "<data:blog.homepageUrl/>";
+        var disqus_blogger_canonical_homepage_url = "<data:blog.canonicalHomepageUrl/>";
+
+        if ($('body[class*="item"]').length > 0) {
+            (function() {
+                var bloggerjs = document.createElement("script");
+                bloggerjs.type = "text/javascript";
+                bloggerjs.async = true;
+                bloggerjs.src = "//" + disqus_shortname + ".disqus.com/blogger_item.js";
+            })();
+            (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(bloggerjs);
+        };
+
+
+        (function() {
+            var bloggerjs = document.createElement("script");
+            bloggerjs.type = "text/javascript";
+            bloggerjs.async = true;
+            bloggerjs.src = "//" + disqus_shortname + ".disqus.com/blogger_index.js";
+        })();
     }
 
     function loadMainScript() {
@@ -21,13 +48,13 @@
     }
 
     $(document).ready(function() {
-    	loadCommentsScript();
-    	loadMainScript();
+        loadCommentsScript();
+        loadMainScript();
         hidePreloader();
 
         // Load comments only if comments are enabled in the page. Reduces console errors and keeps browser happy.
-        if ($('iframe').is('#comment-editor')) {
-        	loadComments();
+        if ($('section').is('#comments')) {
+            loadComments();
         }
     });
 
