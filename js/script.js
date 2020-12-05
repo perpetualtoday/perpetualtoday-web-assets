a285dabb771593a5cd6af6daf048b6f148565de4
John Vincent Corcega <corcegajovic@gmail.com>
2020-12-05 13:36:19 +0800

    Fix wrong syntax 2

--- js/script.js
+++ js/script.js
@@ -203,8 +203,7 @@ $(function() {
                     onepressMoreLinkContentb.find('.index-post').addClass('post-animated post-fadeInUp');
                     console.log(onepressMoreLinkContentb.html());
 
-                    var blogPostsData = onepressMoreLinkContentb.html();
-                    var blogPostsDataTrimmed = blogPostsData.find​('script').remove();
+                    var blogPostsDataTrimmed = onepressMoreLinkContentb.html().find​('script').remove();
                     console.log(blogPostsDataTrimmed);
                     $('.blog-posts').append(onepressMoreLinkContentb.html());
                     onepressMoreLinkContent9 = $(onepressFollowByEmailText5).find('#onepress-free-load-more-link').data('load');
